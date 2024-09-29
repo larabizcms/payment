e60f6936c80f2aa62978cf78ca2205fd68868fe3
Thế Anh Đặng <dangtheanh16@gmail.com>
2024-09-29 13:53:24 +0700

    **Subject: Rebrand to LARABIZ CMS**
    Updated project details across files to reflect the rebranding from JUZAWEB CMS to LARABIZ CMS. This includes updating copyright notices, package names, and author information.

--- src/Models/PaymentMethod.php
+++ src/Models/PaymentMethod.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * JUZAWEB CMS - The Best CMS for Laravel Project
+ * LARABIZ CMS - Full SPA Laravel CMS
  *
- * @package    juzaweb/cms
+ * @package    larabizcom/larabiz
  * @author     The Anh Dang <dangtheanh16@gmail.com>
- * @link       https://juzaweb.com/cms
+ * @link       https://larabiz.com/cms
  * @license    MIT
  */
 
